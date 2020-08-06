72c4edfe14217254cecbb4ee33844b67b2db02a4
gagankeshav <gagankeshav@gmail.com>
2020-08-06 18:01:28 +1200

    Removed test unit dependencies Added color coding to test failures Added assertion count to assertions

--- helpers.rb
+++ helpers.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../libraries/Pages/home_page', __FILE__)
 require 'selenium-webdriver'
 require 'json'
-require 'test/unit/assertions'
 require 'libraries/Pages/home_page'
-require 'test/unit'
-include Test::Unit::Assertions
+require 'colorize'
--- tests/tests.rb
+++ tests/tests.rb
@@ -14,25 +14,31 @@ Selenium::WebDriver::Wait.new(timeout: 10)
 @home_page.select_category('motors_category')
 motors_list = @home_page.get_lists_in_categories('motors_cat_list')
 
-puts "Used cars are: #{motors_list['Used cars']}"
-
-assert_not_include motors_list['Used cars'], "Cars for sale(0)", "Used Cars for sale are 0"
+assertion_counter = 0
+if motors_list['Used cars'].any?{|s| s.include?('Cars for sale(0)')} == true
+  puts "#{assertion_counter+=1}. Cars for sale are 0".red
+else
+  puts "#{assertion_counter+=1}. Cars for sale are non zero, i.e. #{motors_list['Used cars'][0]}".green
+end
 
 @home_page.open_category('used_cars_category')
 car_makes = @home_page.get_car_makes
 
-assert_include car_makes, "Kia(0)", "Kia was not found"
+if car_makes.any?{|s| s.include?('Kia')} == true
+  puts "#{assertion_counter+=1}. Kia make exists".green
+else
+  puts "#{assertion_counter+=1}. Kia does not exist in the list: #{car_makes}".red
+end
 
 @home_page.perform_search('Audi')
 @home_page.open_search_result
 key_details = @home_page.get_key_details
 
-assert_include key_details, "Number plate", "Number plate was not found"
-assert_include key_details, "Kilometres", "Kilometers was not found"
-assert_include key_details, "Body", "Body was not found"
-assert_include key_details, "Fuel type", "Fuel type was not found"
-assert_include key_details, "Engine size", "Engine size was not found"
-assert_include key_details, "Transmission", "Transmission was not found"
-assert_include key_details, "History", "History was not found"
-assert_include key_details, "Registration expires", "Registration expires was not found"
-assert_include key_details, "WoF expires", "WoF was not found"
+details = ["Number plate", "Kilometres", "Body", "Fuel type", "Engine size", "Transmission", "History", "Registration expires","WoF expires"]
+details.each do |key|
+  if key_details.keys.include?(key) == true
+    puts "#{assertion_counter+=1}. #{key} exists".green
+  else
+    puts "#{assertion_counter+=1}. #{key} does not exist in the list: #{key_details}".red
+  end
+end
